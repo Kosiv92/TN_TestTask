0983b8873fd28f0db5cd58c37f2c3f2367011caf
kosiv92@gmail.com <kosiv92@gmail.com>
2023-06-07 00:47:57 +0800

    Add infrastructure project and implement EF Core repository

---
+++ TN_TestTask.Infrastructure/Interfaces/IEfRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TN_TestTask.Core;
+
+namespace TN_TestTask.Infrastructure.Interfaces
+{
+    internal interface IEfRepository<T> 
+        : IBaseRepository<T> where T : BaseEntity
+    {
+        public Task SaveChangesAsync();
+    }
+}
---
+++ TN_TestTask.Infrastructure/Repositories/EfRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TN_TestTask.Core;
+using TN_TestTask.Core.Exceptions;
+using TN_TestTask.Infrastructure.Interfaces;
+
+namespace TN_TestTask.Infrastructure.Repositories
+{
+    internal class EfRepository<T> : IEfRepository<T> where T : BaseEntity
+    {
+        public DbContext _context;
+        public DbSet<T> _dbSet;
+
+        public EfRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<Guid> CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity.Id;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = _dbSet.FirstOrDefault(x => x.Id == id);
+            if (entity == null)
+                throw new NotFoundException(nameof(entity), id);
+
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+           return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Update(T entity)
+        {            
+            _dbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
