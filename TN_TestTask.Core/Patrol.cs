f95bc262b5840acd7acdcebaed5679a8f24eafec
kosiv92@gmail.com <kosiv92@gmail.com>
2023-06-07 00:02:52 +0800

    Add Core project with basic entities

---
+++ TN_TestTask.Core/Patrol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TN_TestTask.Core
+{
+    public class Patrol : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset DateBegin { get; set; }
+
+        public DateTimeOffset DateEnd { get; set; }
+
+    }
+}
