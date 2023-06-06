af61bafbb40d3c3278ac4b4c9357985a923c87db
kosiv92@gmail.com <kosiv92@gmail.com>
2023-06-06 23:54:42 +0800

    Добавьте файлы проекта.

---
+++ TN_TestTask.WebMVC/Controllers/PatrolController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TN_TestTask.WebMVC.Controllers
+{
+    public class PatrolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TN_TestTask.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TN_TestTask.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
